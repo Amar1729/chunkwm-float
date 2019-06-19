0e84d740445548fa43dd2cec9da05a51248bd2c2
Amar1729 <amar.paul16@gmail.com>
2019-06-19 02:14:22 -0400

    massive reorg, stub out command parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // Plugin for better floating window management for chunkwm.
 // created using chunkwm-rs bridge.
 
-use std::env;
+// #[allow(unused_variables)]
 
 #[macro_use]
 extern crate chunkwm;
@@ -9,9 +9,20 @@ extern crate chunkwm;
 use chunkwm::prelude::{ChunkWMError, Event, HandleEvent, LogLevel,
                        Payload, Subscription, API};
 
+macro_rules! c_logger {
+    ($slf:expr, $n:expr, $x:expr) => (
+        // want to take multiple args to fmt tho
+        $slf.api.log(LogLevel::Warn, format!("{}: {}", $n, $x))
+    );
+}
+
+//mod parse;
+
 // Create an event handler. Your handler should be `pub`.
 pub struct Plugin {
     api: API,
+    //step_size_move: CVar<f32>,
+    //step_size_dilate: CVar<f32>,
 }
 
 // Create the bridge between the C/C++ plugin and the event handler.
@@ -30,6 +41,8 @@ impl HandleEvent for Plugin {
 
         Plugin {
             api,
+            //step_size_move: CVar::new("step_size_move", api).unwrap(),
+            //step_size_dilate: CVar::new("step_size_dilate", api).unwrap(),
         }
     }
 
@@ -52,17 +65,9 @@ impl HandleEvent for Plugin {
                 );
             }
             Event::DaemonCommand(data) => {
-                let res = command_handler(data);
-                self.api.log(
-                    LogLevel::Warn,
-                    format!("result: {:?}", res),
-                );
-
-                let args: Vec<String> = env::args().collect();
-                self.api.log(
-                    LogLevel::Warn,
-                    format!("args: {:?}", args),
-                );
+                //let cmd = self.parse_args("--size 0.0 --move west".to_string()).unwrap();
+                let res = self.command_handler(data);
+                c_logger!(self, "result", res);
             }
             _ => (),
         };
@@ -77,32 +82,124 @@ impl HandleEvent for Plugin {
     }
 }
 
-fn command_handler(p: Payload) -> String {
-    // need a nonempty command and message
-    let cmd = match p.command() {
-        Ok(v) => v,
-        Err(e) => {
-            return String::from(format!("chunkwm-float: error: {}", e));
-        }
-    };
+pub struct Command {
+    size: f32,
+    command: String,
+    value: String,
+}
 
-    let args = match p.message() {
-        Ok(v) => v,
-        Err(e) => {
-            return String::from(format!("chunk-float: error: {}", e));
-        }
-    };
+pub trait Handler {
+    fn parse_args(&self, message: String) -> Result<Command, &'static str>;
+    fn command_handler(&self, payload: Payload) -> String;
+}
 
-    //api.log(LogLevel::Warn, format!("recv {}", cmd));
-    match cmd.as_ref() {
-        "window" => {
-            return String::from("window");
+impl Handler for Plugin {
+    fn parse_args(&self, message: String) -> Result<Command, &'static str> {
+        let mut size: f32 = 0.0;
+        let mut cmd: String = String::from("");
+        let mut value: String = String::from("");
+
+        let mut get_size: bool = false;
+        let mut get_cmd: bool = false;
+
+        c_logger!(self, "msg", message);
+
+        for i in message.split(" ") {
+            c_logger!(self, "i", i);
+
+            if get_size && get_cmd {
+                self.api.log(
+                    LogLevel::Warn,
+                    "incorrect args"
+                );
+                return Err("--size needs param");
+            } else if get_size {
+                get_size = false;
+                size = i.parse().unwrap(); // can do error here
+            } else if get_cmd {
+                get_cmd = false;
+                if value != "" {
+                    // passed more than one cmd
+                    self.api.log(
+                        LogLevel::Warn,
+                        "incorrect args"
+                    );
+                    return Err("too many commands");
+                }
+                value = String::from(i);
+            } else if i == "--size" || i == "-s" {
+                get_size = true;
+                get_cmd = false;
+                continue;
+            } else if i == "--move" || i == "-m" {
+                get_size = false;
+                get_cmd = true;
+                cmd = String::from("move");
+            }
         }
-        "query" => {
-            return String::from("query");
-        }
-        e => {
-            return String::from(format!("chunkwm-float: unknown command {}", e));
+
+        Ok(
+            Command {
+                size : size,
+                command : cmd,
+                value : value,
+            }
+        )
+    }
+
+    fn command_handler(&self, payload: Payload) -> String {
+        // need a nonempty command and message
+        let cmd = match payload.command() {
+            Ok(v) => v,
+            Err(e) => {
+                return String::from(format!("chunkwm-float: error: {}", e));
+            }
+        };
+
+        let _args = match payload.message() {
+            Ok(v) => v,
+            Err(e) => {
+                return String::from(format!("chunk-float: error: {}", e));
+            }
+        };
+
+        //api.log(LogLevel::Warn, format!("recv {}", cmd));
+        match cmd.as_ref() {
+            "window" => {
+                return String::from("window");
+            }
+            "query" => {
+                return String::from("query");
+            }
+            e => {
+                return String::from(format!("chunkwm-float: unknown command {}", e));
+            }
         }
     }
 }
+
+/*
+fn window_handler(m: String, plugin: &mut Plugin) -> String {
+    /*
+    let step_size = plugin.step_size_move.value();
+    let action = "";
+    for arg in m.split(" ") {
+        match arg.as_ref() {
+            "--size" => {
+                step_size = arg;
+            }
+    }
+    */
+    let args: Vec<String> = env::args().collect();
+    plugin.api.log(
+        LogLevel::Warn,
+        format!("env: {:?}", args)
+    );
+    return String::from("wut");
+}
+
+fn query_handler(m: String, plugin: &mut Plugin) -> String {
+    return String::from("query");
+}
+*/
+
